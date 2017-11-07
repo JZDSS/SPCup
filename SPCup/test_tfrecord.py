0b029ef3bfc3ed9fb104bf7877ab7a3d647e1fcc
QiYao <qy@stu.scu.edu.cn>
2017-11-08 00:01:05 +0800

    test if .tfrecord is correct

---
+++ SPCup/test_tfrecord.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+def read_from_tfrecord(tfrecord_file_queue):
+    # tfrecord_file_queue = tf.train.string_input_producer(filenames, name='queue')
+    reader = tf.TFRecordReader()
+    _, tfrecord_serialized = reader.read(tfrecord_file_queue)
+    tfrecord_features = tf.parse_single_example(tfrecord_serialized,
+        features={
+            'label': tf.FixedLenFeature([], tf.string),
+            'patch_raw': tf.FixedLenFeature([], tf.string)
+        }, name='features')
+    image = tf.decode_raw(tfrecord_features['patch_raw'], tf.uint8)
+    ground_truth = tf.decode_raw(tfrecord_features['label'], tf.int32)
+
+    image = tf.reshape(image, [64, 64, 3])
+    ground_truth = tf.reshape(ground_truth, [1])
+    return image, ground_truth
+
+image, label = read_from_tfrecord(tf.train.string_input_producer(['../patches/spc_train.tfrecords']))
+with tf.Session() as sess:
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+
+    image, labels = sess.run([image, label])
+    print(labels)
+    plt.imshow(image)
+    plt.show()
+    coord.request_stop()
+    coord.join(threads)
